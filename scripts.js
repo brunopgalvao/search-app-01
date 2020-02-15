c61ac1bdf07f56ed831b24c6cbcafa09de27ab82
Bruno Galvao <brunopgalvao@gmail.com>
2020-02-15 15:53:11 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function search() {
+    let input, filter, main, article, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    main = document.querySelector("main");
+    article = main.getElementsByTagName("article");
+    for (i = 0; i < article.length; i++) {
+        a = article[i].getElementsByTagName("h3")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            article[i].style.display = "";
+        } else {
+            article[i].style.display = "none";
+        }
+    }
+}
